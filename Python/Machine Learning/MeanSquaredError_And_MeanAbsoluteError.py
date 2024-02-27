7fe3d9ab348729f4e675e7735c73f5f6593393eb
MikaelTunturi <63558884+MikaelTunturi@users.noreply.github.com>
2024-02-27 03:43:40 +0200

    Update and rename MLSM, Quiz 1, Question 2, MSE, MAE.py to MeanSquaredError_And_MeanAbsoluteError.py

--- Python/Machine Learning/MeanSquaredError_And_MeanAbsoluteError.py
+++ Python/Machine Learning/MeanSquaredError_And_MeanAbsoluteError.py
@@ -1,3 +1,9 @@
+# Course: Machine Learning Supervised Methods
+# Quiz 1, Assignment 2
+
+# In this exercise, we want to determine which line is a better fit for the 
+# given data points (X,Y) based on Mean Squared Error and Mean Absolute Error.
+
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import numpy as np
 
@@ -6,18 +12,21 @@ Y = [7,1,2,1,2,2,3.5,3.5,5,4,6]
 line1 = lambda x: [element + 1 for element in x]
 line2 = lambda x: [element + 0.5 for element in x]
 
+# A function for Mean Squared Error.
 def my_mse(y_true, y_pred):
     res = 0
     for ii in range(len(y_pred)):
         res += (y_true[ii]-y_pred[ii])**2
     return res/len(y_pred)
 
+# A function for Mean Absolute Error.
 def my_mae(y_true, y_pred):
     res = 0
     for ii in range(len(y_pred)):
         res += np.abs(y_true[ii]-y_pred[ii])
     return res/len(y_pred)
 
+# Let's calculate MSE for lines 1 and 2.
 mse_res1 = my_mse(Y, line1(X))
 mse_res2 = my_mse(Y, line2(X))
 if mse_res1 < mse_res2:
@@ -26,6 +35,7 @@ if mse_res1 < mse_res2:
 else:
     print("Line 2 is a better fit for the data w.r.t. Mean Squared Error!")
 
+# Let's calculate MAE for lines 1 and 2.
 mae_res1 = my_mae(Y, line1(X))
 mae_res2 = my_mae(Y, line2(X))
 if mae_res1 < mae_res2:
